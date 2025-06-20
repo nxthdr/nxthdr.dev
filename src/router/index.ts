3b60e158ca8440b3a605dc287f71b83f40a106ed
matthieugouel <matthieu.gouel@protonmail.com>
2025-06-20 19:47:04 +0200

    setup authentication

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CallbackView from '../views/CallbackView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import { useLogto } from '@logto/vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +13,26 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/callback',
+      name: 'callback',
+      component: CallbackView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      meta: { requiresAuth: true },
     },
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  const { isAuthenticated } = useLogto();
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({ name: 'home' });
+  } else {
+    next();
+  }
+});
 
 export default router
