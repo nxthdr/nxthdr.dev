210b461d731ecbcaf9c10e4e1867154061919536
matthieugouel <matthieu.gouel@protonmail.com>
2025-07-07 13:20:20 +0200

    fix links on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,12 @@
               nxthdr, pronounced "Next Header", is an initiative to offer metrology capabilities to researchers and students to study and learn about the Internet.
             </p>
             <p>
-              It integrates high-speed probing capabilities together with peering control, offering the community a unique platform. The nxthdr <router-link to="/docs">infrastructure</router-link> enables Internet-scale route tracing while managing and collecting BGP advertisements from diverse vantage points and multiple IXPs, powered by the <router-link to="/docs">as215011</router-link> network.
+              It integrates high-speed probing capabilities together with peering control, offering the community a unique platform. The nxthdr <router-link to="/docs/infrastructure">infrastructure</router-link> enables Internet-scale route tracing while managing and collecting BGP advertisements from diverse vantage points and multiple IXPs, powered by the <router-link to="/docs/as215011">as215011</router-link> network.
             </p>
           </div>
 
           <div class="alert alert-important">
-            <p>Currently, nxthdr is in its early stages and not yet open to the public for experiments. However, the <router-link to="/docs">datasets</router-link> collected are already accessible.</p>
+            <p>Currently, nxthdr is in its early stages and not yet open to the public for experiments. However, the <router-link to="/docs/datasets">datasets</router-link> collected are already accessible.</p>
           </div>
 
           <h2 class="subheading">Flagship Projects</h2>
@@ -24,20 +24,20 @@
           <div class="content-section">
             <h3 class="section-title">Peering Platform</h3>
             <p>
-              The peering platform is built around an autonomous system, <router-link to="/docs">as215011</router-link>, which advertises multiple IPv6 prefixes. This network is designed to support researchers and students in conducting Internet-scale experiments on routing and peering.
+              The peering platform is built around an autonomous system, <router-link to="/docs/as215011">as215011</router-link>, which advertises multiple IPv6 prefixes. This network is designed to support researchers and students in conducting Internet-scale experiments on routing and peering.
             </p>
             <p>
-              BGP data is collected using <a href="https://github.com/nxthdr/risotto" target="_blank" rel="noopener">risotto</a> BGP Monitoring Protocol (BMP) collector. Each as215011 router sends BMP messages to risotto, which stores the updates in a ClickHouse database. By maintaining a state, risotto ensures accurate and comprehensive BGP data, which is made freely <router-link to="/datasets">available</router-link> to everyone for analysis.
+              BGP data is collected using <a href="https://github.com/nxthdr/risotto" target="_blank" rel="noopener">risotto</a> BGP Monitoring Protocol (BMP) collector. Each as215011 router sends BMP messages to risotto, which stores the updates in a ClickHouse database. By maintaining a state, risotto ensures accurate and comprehensive BGP data, which is made freely <router-link to="/docs/datasets">available</router-link> to everyone for analysis.
             </p>
           </div>
 
           <div class="content-section">
             <h3 class="section-title">Probing Platform</h3>
             <p>
-              The probing platform consists of multiple servers distributed worldwide. These servers can perform high-speed traceroute-like and ping-like measurements. They advertise <router-link to="/as215011">as215011</router-link> IPv6 prefixes, allowing measurements using both source unicast and anycast addresses.
+              The probing platform consists of multiple servers distributed worldwide. These servers can perform high-speed traceroute-like and ping-like measurements. They advertise <router-link to="/docs/as215011">as215011</router-link> IPv6 prefixes, allowing measurements using both source unicast and anycast addresses.
             </p>
             <p>
-              <a href="https://github.com/nxthdr/saimiris" target="_blank" rel="noopener">saimiris</a> is a high-speed probing pipeline that provides the essential plumbing for users to perform active measurements from the probing servers. The measurements results are stored in a ClickHouse database and made freely <router-link to="/docs">available</router-link> to everyone for analysis.
+              <a href="https://github.com/nxthdr/saimiris" target="_blank" rel="noopener">saimiris</a> is a high-speed probing pipeline that provides the essential plumbing for users to perform active measurements from the probing servers. The measurements results are stored in a ClickHouse database and made freely <router-link to="/docs/datasets">available</router-link> to everyone for analysis.
             </p>
           </div>
 
@@ -45,7 +45,7 @@
           <div class="content-section">
             <p>Every aspect of nxthdr is open, transparent and participatory:</p>
             <ul class="values-list">
-              <li>The <router-link to="/datasets">datasets</router-link> collected is freely accessible under <a href="https://opendatacommons.org/licenses/odbl/" target="_blank" rel="noopener">ODbL</a> license, with no restrictions, delay, authentication, or need for justification.</li>
+              <li>The <router-link to="/docs/datasets">datasets</router-link> collected is freely accessible under <a href="https://opendatacommons.org/licenses/odbl/" target="_blank" rel="noopener">ODbL</a> license, with no restrictions, delay, authentication, or need for justification.</li>
               <li>The <a href="https://github.com/nxthdr" target="_blank" rel="noopener">tools</a> powering the platform are all open-source, such as <a href="https://github.com/nxthdr/risotto" target="_blank" rel="noopener">risotto</a> and <a href="https://github.com/nxthdr/saimiris" target="_blank" rel="noopener">saimiris</a>. You can use them, contribute to them, or even run your own instance.</li>
               <li>The <a href="https://github.com/nxthdr/infrastructure" target="_blank" rel="noopener">infrastructure</a> is managed entirely as open-source code. You can see the configuration of the servers and routers, the services running, the monitoring setup, the database tables and more.</li>
               <li>The <a href="https://grafana.nxthdr.dev/dashboards" target="_blank" rel="noopener">observability</a> is public, with Grafana dashboards providing real-time insights into the platform's health.</li>
